be35ee0367b4c0b6b4d9e93721dc6f2f7488159a
alexanderwjrussell <awjrussell@live.co.uk>
2016-04-12 16:49:54 +0100

    Modified the leaving command for the while loop

--- DeafGrandma.rb
+++ DeafGrandma.rb
@@ -2,7 +2,7 @@ puts "HELLO SONNY BOY. GOOD TO SEE YOU"
   user_input = gets.chomp
   random_year = (1930 + rand(25))
 
-while user_input != 'BYE '
+while user_input != 'BYE'
   user_input = gets.chomp
     if user_input == user_input.upcase
       puts "I REMEMBER THAT BACK IN THE #{random_year.to_s}"
